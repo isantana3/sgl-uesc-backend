513b71f2c2cf128db693a1598214f3fdda81c443
SerraZ3 <serra.henrique3@gmail.com>
2023-05-27 16:46:33 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,7 @@ import { TransformInterceptor } from './interceptors/transform.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    // origin: [
-    //   'http://localhost:3000'
-    // ],
+    origin: ['http://localhost:3000', 'http://localhost:5173/', '*'],
     // methods: ['GET', 'POST'],
     // credentials: true,
   });
