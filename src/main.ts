3b1f2e2d1ebe6e9298d5408fd5fc5ab015547a7a
igsantana3 <iago.gomes@contacomigo.digital>
2024-11-06 20:27:45 -0300

    add config for cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,33 @@
 import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from './interceptors/transform.interceptor';
 import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN || '*',
+    allowedHeaders: ['Content-Type', 'Authorization', 'Xsrf-Token', 'Cookie', 'Access-Control-Allow-Origin'],
+    methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    credentials: true,
+  });
+
+  // Prefixo global para rotas
+  const globalPrefix = process.env.URL_PREFIX || 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  // // Configuração do Swagger
+  // const config = new DocumentBuilder()
+  //   .setTitle('SGL-UESC')
+  //   .setDescription('Sistema de Gestão de Laboratórios da UESC (SGL-UESC)')
+  //   .setVersion('1.0')
+  //   .build();
+  // const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup(${globalPrefix}/docs, app, document);
 
   app.use(cookieParser());
 
@@ -37,6 +60,10 @@ async function bootstrap() {
     }
     next();
   });
+
+  // Pipes globais e interceptors
+  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
 
   await app.listen(3333);
 }
